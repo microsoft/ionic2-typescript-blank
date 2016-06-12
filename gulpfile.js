03b918f9265d4f96bc55564874f8954d77eb7db5
NewtonJain <newton.jain@gmail.com>
2016-06-11 21:42:50 -0700

    Fixed rxjs version issue and added default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ var gulp = require('gulp'),
  */
 gulp.task('serve:before', ['watch']);
 
+gulp.task('default', ['build']);
+
 // we want to 'watch' when livereloading
 var shouldWatch = argv.indexOf('-l') > -1 || argv.indexOf('--livereload') > -1;
 gulp.task('run:before', [shouldWatch ? 'watch' : 'build']);
